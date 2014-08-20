eb36e4d26346ce4f70ed19fdd2ec7894e3474e56
Kevin <kevin@ubuntu.(none)>
2014-08-21 00:48:05 +0100

    Change update script to work with wget

--- scripts/checkupdates.sh
+++ scripts/checkupdates.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-UPDATE_URL=www.gamoeba.com/stagehand_updates
+UPDATE_URL="http://www.gamoeba.com/stagehand_updates"
 PLATFORM=`uname`
-AVAIL_VERSION=`curl -s $UPDATE_URL/version_$PLATFORM.txt`
+AVAIL_VERSION=`wget -qO- $UPDATE_URL/version_$PLATFORM.txt`
 CURRENT_VERSION=`cat $DIR/CURRENT_VERSION`
 NEWVER=`echo $AVAIL_VERSION'>'$CURRENT_VERSION | bc -l`
 
 if (( $NEWVER == 1 ));then
 	echo "update available. fetching $AVAIL_VERSION"
 	UPDATE_FILE=$PLATFORM"_update"$AVAIL_VERSION".zip"
-	curl $UPDATE_URL/$UPDATE_FILE -o $DIR/$UPDATE_FILE && unzip $DIR/$UPDATE_FILE && rm $DIR/$UPDATE_FILE
+	wget $UPDATE_URL/$UPDATE_FILE -O $DIR/$UPDATE_FILE && unzip -o $DIR/$UPDATE_FILE && rm $DIR/$UPDATE_FILE
 fi
